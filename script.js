3535001c63d03dd861efe0b639bac3390cee9f10
Guilherme Nogara <gnogara@gmail.com>
2019-03-22 01:32:58 -0300

    New quer button

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
 document.getElementById('date').innerHTML = new Date().toDateString();
+var CNPJ = document.getElementById('usrInput').value;
+console.log(CNPJ); //Ver no log oq ta saindo
+function output(){
+    var input = CNPJ;
+alert(input);
+}
