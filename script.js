d14791c3e1f908a9fc062e124a7b5e685cf4cd8d
g-nogara <48070989+g-nogara@users.noreply.github.com>
2019-03-22 18:34:46 -0300

    new files uploaded. html->index

--- script.js
+++ script.js
@@ -1,7 +1,5 @@
 document.getElementById('date').innerHTML = new Date().toDateString();
-var CNPJ = document.getElementById('usrInput').value;
-console.log(CNPJ); //Ver no log oq ta saindo
-function output(){
-    var input = CNPJ;
-alert(input);
-}
+ function showInput() {
+        document.getElementById('display').innerHTML =
+                    document.getElementById("user_input").value;
+ }
