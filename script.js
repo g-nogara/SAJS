45e5e951eb477fd4dc6d61b56e794890bcf12f8a
Guilherme Nogara <gnogara@gmail.com>
2019-04-03 00:47:09 -0300

    New localStorage function

--- script.js
+++ script.js
@@ -9,5 +9,16 @@ function getCNPJ() {
         result.textContent = "Seu CNPJ é: " + cnpj;
     }
 }
+function lclStg () {
+    let realCNPJ = document.getElementById('cnpj').value;
+    const result = document.getElementById('result');
+
+    if (cnpj.length < 14 || cnpj.length > 18 ) {
+        result.textContent = "CNPJ deve conter entre 14 e 18 digitos";
+    } 
+    else {
+        window.localStorage.setItem('CNPJ', cnpj);
+        result.textContent = "CNPJ salvo com sucesso";
+}
 subButton = document.getElementById('subButton');
-subButton.addEventListener('click', getCNPJ, false); 
+subButton.addEventListener('click', lclStg, false); 
