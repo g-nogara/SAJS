dfeed15131695495c431cbac1699d1b81568a6e8
Guilherme Nogara <gnogara@gmail.com>
2019-04-22 02:25:01 -0300

    New <p> with id=result to show success/fail state

--- script.js
+++ script.js
@@ -1,12 +1,14 @@
-function loginCheck () {
+function loginCheck() {
     const realLogin = document.getElementById('login').value;
     const realPWD = document.getElementById('password').value;
+    const result = document.getElementById('result');
 
-    if (realLogin === "Admin" && realPWD === "senha" ) {
-        window.location.href = 'http://www.google.com';
-    } 
+    if (realLogin === "Admin" && realPWD === "senha") {
+        window.location = 'http://www.google.com';
+    }
     else {
         result.textContent = "Login/Senha Inválidos";
+    }
+    botaoAcesso = document.getElementById('botaoAcesso');
+    botaoAcesso.addEventListener('click', loginCheck, false); 
 }
-botaoAcesso = document.getElementById('botaoAcesso');
-botaoAcesso.addEventListener('click', loginCheck, false); 
