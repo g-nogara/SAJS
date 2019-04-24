d30683e87fb3fffed20fcad731ee90774d98ab73
g-nogara <48070989+g-nogara@users.noreply.github.com>
2019-04-24 18:02:17 -0300

    Function gravar e recuperar funcionam
    Função gravar grava e recuperar recupera.

--- Estoque/scriptEstoque.js
+++ Estoque/scriptEstoque.js
@@ -1,4 +1,4 @@
-const itens = {
+let itens = {
     inserir: function (codig, quantidade, valor, describe) {
         this.codig = codig;
         this.quantidade = quantidade;
@@ -6,21 +6,23 @@ const itens = {
         this.describe = describe;
     }
 };
-let index = [];
+let array2 = [];
 function gravar(){
   const codigo = document.getElementById("codigo").value, quantidade = document.getElementById("quantidade").value,
   valor = document.getElementById("valor").value, describe = document.getElementById("describe").value
   itens.inserir(codigo, quantidade, valor, describe);
-  console.log(itens);
   const itemJSON = JSON.stringify(itens);
   console.log(itemJSON);
-  sessionStorage.setItem(itens.codigo, itemJSON);
-  index[index.length + 1] = itens.codigo;
+  console.log(itens);
+  sessionStorage.setItem(itens.codig, itemJSON);
+  array2.push(itens.codig);
+  console.log(array2);
 
 };
 
 function recuperar(){
-  const ojb = sessionStorage.getItem(index[variavel]);
+  const ojb = sessionStorage.getItem(array2[parseInt(prompt("Informe o código do produto"))]);
   console.log(JSON.parse(ojb));
+  document.getElementById("exibir").innerHTML+= ojb;
 
 };
