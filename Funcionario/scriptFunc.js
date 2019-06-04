e63d43ef4b6b2cca539fe3234cb30a895100e01e
g-nogara <48070989+g-nogara@users.noreply.github.com>
2019-06-03 21:21:27 -0300

    Nova página cadastro de funcionários

---
+++ Funcionario/scriptFunc.js
@@ -0,0 +1,28 @@
+function Funcionario(nome, cod, cargo, admissao){
+    this.nome = nome;
+    this.cod = cod;
+    this.cargo = cargo;
+    this.dataAdmissao = admissao;
+}
+
+function gravar(){
+  getElementsHTML();
+  const novoFunc = new Funcionario (nome.value, codigo.value, cargo.value, admissao.value);
+  const funcJSON = JSON.stringify(novoFunc);
+  sessionStorage.setItem(novoFunc.cod, funcJSON);
+
+
+  function getElementsHTML() {
+    const nome = document.getElementById("nome"),
+    codigo = document.getElementById("codigo"),
+    cargo = document.getElementById("cargo"),
+    admissao = document.getElementById("admissao");
+  }
+};
+
+function recuperar(){
+  const ojb = sessionStorage.getItem(parseInt(prompt("Informe o código do produto")));
+  console.log(JSON.parse(ojb));
+  document.getElementById("exibir").innerHTML+= ojb;
+
+};
