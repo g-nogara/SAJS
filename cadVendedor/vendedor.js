0068da1cadbc272fd502ee0dd452ac8e1d6ee6fc
Guilherme Nogara <gnogara@gmail.com>
2019-06-06 14:54:41 -0300

    Padronização de index/estoque/funcionario/venda

--- cadVendedor/vendedor.js
+++
@@ -1,7 +0,0 @@
-function salvarDados(){
-    const nome = document.getElementById("nome").value,
-    codigo = document.getElementById("codigo").value,
-    cargo = document.getElementById("cargo").value,
-    dataAdmissao = document.getElementById("admissao").value;
-    sessionStorage.setItem(codigo, [nome,cargo,dataAdmissao]);
-}
