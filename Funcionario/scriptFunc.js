948214ca9365997ddd4bee36831360feb2c57bb2
g-nogara <48070989+g-nogara@users.noreply.github.com>
2019-06-06 15:24:14 -0300

    HTML/CSS padronizado (#4)
    * Rebase develop to master
    
    Única forma de criar uma pasta nova sem baixar e configurar todo git dnv
    
    * Setup ./Vendas
    
    Setup básico para mockup
    
    * Tentativa escrever página
    
    innerHTML não funciona ainda
    
    * Estoque atualizado
    
    * Princípio de estoque
    
    Começo de página de estoque
    Cypress adicionado ao gitignore
    
    * New html template
    
    Using bootstrap
    
    * Nova página cadastro de funcionários
    
    * getElementsHTML return with .value
    
    * Reorganizando local páginas
    
    * Ups
    
    * New Index with logo
    
    And footer as a card
    
    * Padronização de index/estoque/funcionario/venda

---
+++ Funcionario/scriptFunc.js
@@ -0,0 +1,28 @@
+function Funcionario(nome, cod, cargo, admissao){
+    this.nome = nome;
+    this.cod = cod;
+    this.cargo = cargo;
+    this.dataAdmissao = admissao;
+}
+
+function gravar(){
+  getElementsHTML();
+  const novoFunc = new Funcionario (nome, codigo, cargo, admissao);
+  const funcJSON = JSON.stringify(novoFunc);
+  sessionStorage.setItem(novoFunc.cod, funcJSON);
+
+
+  function getElementsHTML() {
+    const nome = document.getElementById("nome").value,
+    codigo = document.getElementById("codigo").value,
+    cargo = document.getElementById("cargo").value,
+    admissao = document.getElementById("admissao").value;
+  }
+};
+
+function recuperar(){
+  const ojb = sessionStorage.getItem(parseInt(prompt("Informe o código do produto")));
+  console.log(JSON.parse(ojb));
+  document.getElementById("exibir").innerHTML+= ojb;
+
+};
