dd6d0e7eb5e4f0c8910d92475208e61d4ed87359
Guilherme Nogara <gnogara@gmail.com>
2019-06-24 11:26:22 -0300

    Alteração de domínio entre estoque/venda

--- Vendas/scriptVenda.js
+++ Vendas/scriptVenda.js
@@ -1,29 +1,36 @@
-function Venda(funcionario, cliente, codigo, quantidade){
+function Venda(funcionario, cliente, codigo, itens, quantidade, data){
     this.funcionario = funcionario;
     this.cliente = null || cliente;
     this.codigo = codigo;
+    this.itens = itens;
     this.quantidade = quantidade;
+    this.data = data;
 }
 
-function vender(){
-    let { funcionario, cliente, codigo, quantidade } = getHTMLInputs();
+function vender() {
+  let { funcionario, cliente, codigo, quantidade } = getHTMLInputs();
 
-    let novaVenda = new Venda(funcionario, cliente, codigo, quantidade);
-    novaVenda = JSON.stringify(novaVenda);
-    if (localStorage.length = 0) alert("Sem itens em estoque!");
-    else{
-          var itemString = "";
-          codigo = Array.from(codigo);
-          for (i=0; i<codigo.length; i++){
-            codigo[i] = String(codigo[i].value);
-            codigo[i] = codigo[i].padStart(3,"0");
-            codigo[i] = "e" + codigo[i];
+  if (localStorage.length = 0) alert("Sem itens em estoque!");
+  else {
+    var itemString = "", itemQuantidades = "";
+    codigo = Array.from(codigo);
+    for (i = 0; i < codigo.length; i++) {
+      codigo[i] = String(codigo[i].value);
+      codigo[i] = codigo[i].padStart(3, "0");
+      codigo[i] = "e" + codigo[i];
+      //Padrão armazenamento do estoque no localStorage, "e" seguido pelo código com 3 digitos.
 
-            var itemObj = JSON.parse(localStorage.getItem(codigo[i]));
-            itemString += itemObj.describe + " ";
-          }
-          console.log(itemString);
+      var itemObj = JSON.parse(localStorage.getItem(codigo[i]));
+      itemString += itemObj.describe + ", ";
     }
+    let novaVenda = new Venda(funcionario, cliente, codigo, itemString, Array.from(quantidade), new Date());
+    novaVenda = JSON.stringify(novaVenda);
+    if (localStorage.getItem("vendas") == null) localStorage.setItem("vendas", [novaVenda])
+    else {
+      tempArrayVendas = localStorage.getItem("vendas");
+      tempArrayVendas.push(novaVenda);
+    }
+  }
 }
 
 function getHTMLInputs() {
