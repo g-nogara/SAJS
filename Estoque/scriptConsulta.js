494ecc6aa8c1266f718d1c04646a54fb8e6f5897
Carlosouzapsi <49699545+Carlosouzapsi@users.noreply.github.com>
2019-06-23 21:51:40 -0300

    Do ramo TelasLinkas -> Master
    Telas com link html uma para outra.

---
+++ Estoque/scriptConsulta.js
@@ -0,0 +1,79 @@
+function deletaItem(key){
+    key = String(key);
+    key = key.padStart(3,"0");
+    key = "e" + key;
+    let confirma = prompt("Tem certeza que deseja deletar? \n Informe o código do item para confirmar");
+    confirma = "e" + confirma;
+    console.log(key + " confirma:" + confirma);
+    if (confirma === key){
+        localStorage.removeItem(key);
+        alert("Item deletado com sucesso");
+        location.reload();
+    } else alert("Item não removido")
+
+}
+
+function atualizaItem(key){
+    key = String(key);
+    key = key.padStart(3,"0");
+    key = "e" + key;
+    itemAtualizado = JSON.parse(localStorage.getItem(key));
+    itemAtualizado.valor = parseFloat(prompt("Informe novo valor"));
+    itemAtualizado.quantidade = parseInt(prompt("Informe nova quantidade"));
+    itemAtualizado.describe = prompt("Informe nova descrição");
+    localStorage.setItem(key,JSON.stringify(itemAtualizado));
+    location.reload();
+}
+
+function insereItens() {
+    let { table, buttonUpdate, buttonDelete } = criaElementosHTML();
+
+    if(localStorage.length === 0) alert("Sem itens em estoque!")
+    else for (i=0; i<localStorage.length; i++){
+        let itemEstoque = JSON.parse(localStorage.getItem(localStorage.key(i)));
+        let { cell0, cell1, cell2, cell3, cell4 } = elementosTabela();
+        insereValoresTabela(itemEstoque, cell0, cell1, cell2, cell3, cell4);
+    }
+
+    function insereValoresTabela(itemEstoque, cell0, cell1, cell2, cell3, cell4) {
+        cell0.innerHTML = itemEstoque.codigo;
+        cell1.innerHTML = itemEstoque.valor;
+        cell2.innerHTML = itemEstoque.quantidade;
+        cell3.innerHTML = itemEstoque.describe;
+        buttonUpdate.setAttribute("onclick", "atualizaItem(" + itemEstoque.codigo + ")")
+        cell4.append(buttonUpdate);
+        cell4.innerHTML += " / ";
+        buttonDelete.setAttribute("onclick", "deletaItem(" + itemEstoque.codigo + ")")
+        cell4.append(buttonDelete);
+        cell4.innerHTML +="";
+    }
+
+    function elementosTabela() {
+        let row = table.insertRow(-1);
+        let cell0 = row.insertCell(0);
+        let cell1 = row.insertCell(1);
+        let cell2 = row.insertCell(2);
+        let cell3 = row.insertCell(3);
+        let cell4 = row.insertCell(4);
+        return { cell0, cell1, cell2, cell3, cell4 };
+    }
+
+    function criaElementosHTML() {
+        let table = document.getElementById("tabelaEstoque");
+        let onClickDelete = document.createAttribute("onclick");
+
+        let buttonDelete = document.createElement("button");
+        buttonDelete.innerHTML = "Deletar";
+        buttonDelete.classList.add("btn", "btn-danger", "btn-sm");
+        buttonDelete.setAttributeNode(onClickDelete);
+        
+        let onClickUpdate = document.createAttribute("onclick");
+        let buttonUpdate = document.createElement("button");
+        buttonUpdate.innerHTML = "Atualizar";
+        buttonUpdate.classList.add("btn", "btn-primary", "btn-sm");
+        buttonUpdate.setAttributeNode(onClickUpdate);
+        return { table, buttonUpdate, buttonDelete };
+    }
+}
+
+window.onload = insereItens;
