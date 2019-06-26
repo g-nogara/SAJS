004327cbcad06c0d1ce16a74d5ee6b84f584e36f
Guilherme <gnogara@gmail.com>
2019-06-26 20:02:33 -0300

    Funcionário agora cria um código único para si
    :rocket:

--- Funcionario/scriptFunc.js
+++ Funcionario/scriptFunc.js
@@ -7,19 +7,18 @@ function Funcionario(nome, codigo, cargo, admissao){
 
 function gravar(){
   getElementsHTML();
-  const novoFunc = new Funcionario (nome.value, codigo.value, cargo.value, admissao.value);
+  const codigo = Math.random().toFixed(6).toString().substring(2);
+  const novoFunc = new Funcionario (nome.value, codigo, cargo.value, admissao.value);
   const funcJSON = JSON.stringify(novoFunc);
   localStorage.setItem("f" + novoFunc.codigo, funcJSON);
   nome.value = null;
-  codigo.value = null;
   cargo.value = null;
   admissao.value = null;
-  alert("Salvo com sucesso!");
+  alert("Salvo com sucesso! \n O código do funcionário é " + codigo);
 
 
   function getElementsHTML() {
     const nome = document.getElementById("nome"),
-    codigo = document.getElementById("codigo"),
     cargo = document.getElementById("cargo"),
     admissao = document.getElementById("admissao");
   }
